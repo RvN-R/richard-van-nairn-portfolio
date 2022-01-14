9df80da0e556045d0aa1abb9b0f09e0f2b2b93ae
RvN-R <rvannairn@googlemail.com>
2022-01-14 19:41:36 +0000

    Improved performance of the site on smaller devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Below function controls which divs appear on devices depending on there screen sizes
 $(document).ready(function() {
   // This will fire when document is ready:
   $(window).resize(function() {
@@ -6,35 +7,48 @@ $(document).ready(function() {
           // if smaller or equal
           $('#top-row').hide();
           $('#bottom-row').hide();
+          $('#top-milestone-1').hide();
+          $('#top-milestone-1-md').show();
+          $('#top-milestone-2').hide();
+          $('#top-milestone-2-md').show();
+          
       } else {
           // if larger
           $('#top-row').show();
           $('#bottom-row').show();
+          $('#top-milestone-1').show();
+          $('#top-milestone-1-md').hide();
+          $('#top-milestone-2').show();
+          $('#top-milestone-2-md').hide();
       }
   }).resize(); // This will simulate a resize to trigger the initial run.
 });
 
 $(document).ready(function () {
-
   // Inital start hide bottom layers of all milestones and storys
   $("#bottom-milestone-1-lg").hide();
-  $("#bottom-milestone-2").hide();
+  $("#bottom-milestone-1-md").hide();
+  $("#bottom-milestone-2-md").hide();
+  $("#bottom-milestone-2-lg").hide();
   $("#bottom-milestone-3").hide();
   $("#bottom-milestone-4").hide();
   $("#bottom-my-story").hide();
 
   //Following JS is using fadeIn and fadeOut to create roll over effect or top-row and bottom-row
   $("#top-row").mouseenter(function(){
-    $("#top-row").fadeOut(1500);
+    $("#top-row").fadeOut(1000);
   });
   
   $("#bottom-row").mouseout(function(){
-    $("#top-row").fadeIn(1500);
+    $("#top-row").fadeIn(1000);
   });
 
   $("#top-my-story").click(function(){
     $("#bottom-my-story").slideToggle();
-    $("#bottom-milestone-2").slideUp();
+    $("#bottom-milestone-1-lg").slideUp();
+    $("#bottom-milestone-1-md").slideUp();
+    $("#bottom-milestone-2-lg").slideUp();
+    $("#bottom-milestone-2-md").slideUp();
     $("#bottom-milestone-3").slideUp();
     $("#bottom-milestone-4").slideUp();
   });
@@ -43,15 +57,38 @@ $(document).ready(function () {
   // Follow JS uses click and slide toggle to show off milestone information
   $("#top-milestone-1").click(function(){
     $("#bottom-milestone-1-lg").slideToggle();
-    $("#bottom-milestone-2").slideUp();
+    $("#bottom-milestone-2-lg").slideUp();
+    $("#bottom-milestone-2-md").slideUp();
+    $("#bottom-milestone-1-md").slideUp();
     $("#bottom-milestone-3").slideUp();
     $("#bottom-milestone-4").slideUp();
     $("#bottom-my-story").slideUp();
   });
-  
-  $("#top-milestone-2").click(function(){
-    $("#bottom-milestone-2").slideToggle();
+
+  $("#top-milestone-1-md").click(function(){
+    $("#bottom-milestone-1-md").slideToggle();
     $("#bottom-milestone-1-lg").slideUp();
+    $("#bottom-milestone-2-lg").slideUp();
+    $("#bottom-milestone-2-md").slideUp();
+    $("#bottom-milestone-3").slideUp();
+    $("#bottom-milestone-4").slideUp();
+    $("#bottom-my-story").slideUp();
+  });
+
+  $("#top-milestone-2").click(function(){
+    $("#bottom-milestone-2-lg").slideToggle();
+    $("#bottom-milestone-1-lg").slideUp();
+    $("#bottom-milestone-2-md").slideUp();
+    $("#bottom-milestone-1-md").slideUp();
+    $("#bottom-milestone-3").slideUp();
+    $("#bottom-milestone-4").slideUp();
+    $("#bottom-my-story").slideUp();
+  });
+
+  $("#top-milestone-2-md").click(function(){
+    $("#bottom-milestone-2-md").slideToggle();
+    $("#bottom-milestone-1-lg").slideUp();
+    $("#bottom-milestone-1-md").slideUp();
     $("#bottom-milestone-3").slideUp();
     $("#bottom-milestone-4").slideUp();
     $("#bottom-my-story").slideUp();
@@ -59,8 +96,10 @@ $(document).ready(function () {
 
   $("#top-milestone-3").click(function(){
     $("#bottom-milestone-3").slideToggle();
-    $("#bottom-milestone-2").slideUp();
+    $("#bottom-milestone-2-lg").slideUp();
     $("#bottom-milestone-1-lg").slideUp();
+    $("#bottom-milestone-2-md").slideUp();
+    $("#bottom-milestone-1-md").slideUp();
     $("#bottom-milestone-4").slideUp();
     $("#bottom-my-story").slideUp();
   });
@@ -68,7 +107,9 @@ $(document).ready(function () {
   $("#top-milestone-4").click(function(){
     $("#bottom-milestone-4").slideToggle();
     $("#bottom-milestone-1-lg").slideUp();
-    $("#bottom-milestone-2").slideUp();
+    $("#bottom-milestone-2-lg").slideUp();
+    $("#bottom-milestone-2-md").slideUp();
+    $("#bottom-milestone-1-md").slideUp();
     $("#bottom-milestone-3").slideUp();
     $("#bottom-my-story").slideUp();
   });
