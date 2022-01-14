fcbcf15ecba4c96c7820e30661e2d8f107325b04
RvN-R <rvannairn@googlemail.com>
2022-01-14 09:52:16 +0000

    My Story section added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   $("#bottom-milestone-2").hide();
   $("#bottom-milestone-3").hide();
   $("#bottom-milestone-4").hide();
+  $("#bottom-my-story").hide();
 
   //Following JS is using fadeIn and fadeOut to create roll over effect or top-row and bottom-row
   $("#top-row").mouseenter(function(){
@@ -14,12 +15,21 @@ $(document).ready(function () {
     $("#top-row").fadeIn(1500);
   });
 
+  $("#top-my-story").click(function(){
+    $("#bottom-my-story").slideToggle();
+    $("#bottom-milestone-2").slideUp();
+    $("#bottom-milestone-3").slideUp();
+    $("#bottom-milestone-4").slideUp();
+  });
+
+
   // Follow JS uses click and slide toggle to show off milestone information
   $("#top-milestone-1").click(function(){
     $("#bottom-milestone-1").slideToggle();
     $("#bottom-milestone-2").slideUp();
     $("#bottom-milestone-3").slideUp();
     $("#bottom-milestone-4").slideUp();
+    $("#bottom-my-story").slideUp();
   });
   
   $("#top-milestone-2").click(function(){
@@ -27,6 +37,7 @@ $(document).ready(function () {
     $("#bottom-milestone-1").slideUp();
     $("#bottom-milestone-3").slideUp();
     $("#bottom-milestone-4").slideUp();
+    $("#bottom-my-story").slideUp();
   });
 
   $("#top-milestone-3").click(function(){
@@ -34,6 +45,7 @@ $(document).ready(function () {
     $("#bottom-milestone-2").slideUp();
     $("#bottom-milestone-1").slideUp();
     $("#bottom-milestone-4").slideUp();
+    $("#bottom-my-story").slideUp();
   });
 
   $("#top-milestone-4").click(function(){
@@ -41,6 +53,7 @@ $(document).ready(function () {
     $("#bottom-milestone-1").slideUp();
     $("#bottom-milestone-2").slideUp();
     $("#bottom-milestone-3").slideUp();
+    $("#bottom-my-story").slideUp();
   });
 
   
